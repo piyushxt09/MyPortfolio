63c1b44224883f0f3e165edf0e8a43ed05cff9f4
Priyanshu Chauhan <152474798+piyushxt09@users.noreply.github.com>
2024-08-07 14:19:17 +0530

    Update Projects.jsx

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -6,21 +6,22 @@ import './../Portfolio/Home.css';
 export default function Projects() {
   const ProjectsList = [
     { id: 1, Title: 'PortfolioBuilding Website', Link: '' },
-    { id: 2, Title: 'Netflix Clone', Link: '' },
+    { id: 2, Title: 'Latest Movies', Link: 'https://latest-moviext09.netlify.app/' },
     { id: 3, Title: 'Google Play Store', Link: '' },
-    { id: 4, Title: 'To-do List', Link: '' },
+    { id: 4, Title: 'To-do List', Link: 'https://todolistxt09.netlify.app/' },
     { id: 5, Title: 'E-commerce Website', Link: '' },
-    { id: 6, Title: 'Event Planner Website', Link: '' },
+    { id: 6, Title: 'Conference Expense Planner', Link: 'https://conference-expense-plannerxt09.netlify.app/' },
   ];
-  const EventHandle = (e) => {
-    e.preventDefault();
-  }
+
   return (
     <>
       <section className="Projects" id='Projects'>
         <h1><u>Projects</u></h1>
         <div className="Project-Items">
-          {ProjectsList.map((item) => <div key={item.id} className='Pitem'><a onClick={EventHandle} href={item.Link}>{item.Title}</a></div>)}
+          {ProjectsList.map((item) => 
+          <div key={item.id} className='Pitem'>
+            <a href={item.Link}>{item.Title}</a>
+            </div>)}
         </div>
       </section>
     </>
